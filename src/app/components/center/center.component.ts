91a0d4a8864004bfc7d9b1f239ddbc0cf944b6ba
Sanjay Samantra <sanjaysamantra1@gmail.com>
2024-04-13 21:12:26 +0530

    april 13

---
+++ src/app/components/center/center.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-center',
+  standalone: true,
+  imports: [],
+  templateUrl: './center.component.html',
+  styleUrl: './center.component.css'
+})
+export class CenterComponent {
+
+}

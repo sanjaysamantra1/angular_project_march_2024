880e25b2c809fc2a221b067f80e8661803b21a63
Sanjay Samantra <sanjaysamantra1@gmail.com>
2024-04-17 21:18:36 +0530

    april 17

--- src/app/components/databinding/databinding.component.ts
+++ src/app/components/databinding/databinding.component.ts
@@ -12,11 +12,19 @@ export class DatabindingComponent {
   name: string = 'Sachin Tendulkar';
   img_add: string = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPDUGFX4Ota4MCp2hwoo51UnK-IbukmhEP74CKpX38LA&s'
   flag: boolean = false;
+  msg: string = '';
+  selectedState: string = 'Odisha';
+  num1: number = 0;
+  num2: number = 0;
+  addMessage = '';
 
   f1() {
     alert('This is F1.. function')
   }
   toggle() {
     this.flag = !this.flag;
+  }
+  addition(a: string, b: string) {
+    this.addMessage = `Addition of ${a} and ${b} is ${+a + +b}`
   }
 }

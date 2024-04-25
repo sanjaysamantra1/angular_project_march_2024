864ec4824ca3a4fca3248de37ae8bf93f42590f1
Sanjay Samantra <sanjaysamantra1@gmail.com>
2024-04-25 21:11:10 +0530

    comp communication

--- src/app/components/child2/child2.component.ts
+++ src/app/components/child2/child2.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-child2',
@@ -6,10 +6,11 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './child2.component.html',
   styleUrl: './child2.component.css',
-  inputs: ['b','child1Obj','parentAddress']
+  inputs: ['child1Obj', 'parentAddress']
 })
 export class Child2Component {
-  b: any;
-  child1Obj:any;
-  parentAddress:any;
+  @Input() b: any;
+  
+  child1Obj: any;
+  parentAddress: any;
 }

2f55651b286bfccebd9683f10267e49bcc15b2d0
Sanjay Samantra <sanjaysamantra1@gmail.com>
2024-04-26 21:29:24 +0530

    april 26

--- src/app/components/child1/child1.component.ts
+++ src/app/components/child1/child1.component.ts
@@ -1,15 +1,18 @@
-import { Component, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-child1',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './child1.component.html',
   styleUrl: './child1.component.css',
-  inputs: ['aChild'],
-  outputs: ['nameEvent']
+  inputs: ['aChild', 'arr'],
+  outputs: ['nameEvent'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class Child1Component {
+export class Child1Component implements OnChanges, OnInit {
+  arr: any;
   aChild: any;
   name: string = 'Sanjay Samantra';
   address: string = 'Bangalore';
@@ -17,4 +20,38 @@ export class Child1Component {
   sendDataToParent() {
     this.nameEvent.emit({ name: this.name, address: this.address });
   }
+
+  constructor(private changeDetectorRef: ChangeDetectorRef) {
+    console.log("Child constructor")
+  }
+  refreshMyData() {
+    this.changeDetectorRef.markForCheck();
+  }
+  ngOnInit() {
+    console.log('Child ngOnInit');
+  }
+  ngOnChanges(myChanges: SimpleChanges) {
+    console.log(myChanges)
+    console.log(myChanges['aChild'].currentValue,)
+    console.log(myChanges['aChild'].previousValue,)
+    console.log('Child ngOnChanges');
+  }
+  // ngDoCheck() {
+  //   console.log('Child ngDoCheck');
+  // }
+  // ngAfterContentInit() {
+  //   console.log('Child ngAfterContentInit');
+  // }
+  // ngAfterContentChecked() {
+  //   console.log('Child ngAfterContentChecked')
+  // }
+  // ngAfterViewInit() {
+  //   console.log('Child ngAfterViewInit');
+  // }
+  // ngAfterViewChecked() {
+  //   console.log('Child ngAfterViewChecked');
+  // }
+  // ngOnDestroy() {
+  //   console.log('Child ngOnDestory');
+  // }
 }
--- src/app/components/parent/parent.component.ts
+++ src/app/components/parent/parent.component.ts
@@ -1,21 +1,61 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Child1Component } from '../child1/child1.component';
 import { Child2Component } from '../child2/child2.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-parent',
   standalone: true,
-  imports: [Child1Component, Child2Component],
+  imports: [Child1Component, Child2Component, HttpClientModule, FormsModule, CommonModule],
   templateUrl: './parent.component.html',
-  styleUrl: './parent.component.css'
+  styleUrl: './parent.component.css',
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ParentComponent {
-  a = 100;
-  b = 200;
+  arr = [10, 20, 30, 40]
+  a: number;
+  b: number;
   parentName: any;
   parentAddress: any;
   receiveNameFromChild(data: any) {
     this.parentName = data.name;
     this.parentAddress = data.address;
   }
+
+  constructor(private http: HttpClient) {
+    this.a = 100;
+    this.b = 200;
+    console.log("Parent constructor")
+  }
+  ngOnInit() {
+    console.log('Parent ngOnInit');
+
+    // API calls
+    this.http.get('https://jsonplaceholder.typicode.com/users').subscribe(data => {
+      console.log(data)
+    });
+  }
+  ngOnChanges() {
+    console.log('Parent ngOnChanges');
+  }
+  ngDoCheck() {
+    console.log('Parent ngDoCheck');
+  }
+  // ngAfterContentInit() {
+  //   console.log('Parent ngAfterContentInit');
+  // }
+  // ngAfterContentChecked() {
+  //   console.log('Parent ngAfterContentChecked')
+  // }
+  // ngAfterViewInit() {
+  //   console.log('Parent ngAfterViewInit');
+  // }
+  // ngAfterViewChecked() {
+  //   console.log('Parent ngAfterViewChecked');
+  // }
+  // ngOnDestroy() {
+  //   console.log('Parent ngOnDestory');
+  // }
 }
